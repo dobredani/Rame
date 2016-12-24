0edaa27ca04017e440c9d9ba7df26c80b693d175
dobredani <dobre.dani.gtalk@gmail.com>
2016-12-24 22:13:40 +0200

    Game Menu
    Pentru Ana sau Ioana

---
+++ include/PublicStructures.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <SDL.h>
+
+#define GAME_DIFFICULTY_EASY 1;
+#define GAME_DIFFICULTY_MEDIUM 2;
+#define GAME_DIFFICULTY_HARD 3;
+
+using namespace std;
+
+struct Player{
+    string sName;
+
+    SDL_Keycode xLeftKeyCode; // refer to following page for SDL_Keycode documentation:
+    SDL_Keycode xRightKeyCode; // https://wiki.libsdl.org/SDL_Keycode
+    bool bIsAIplayer;
+    };
---
+++ src/GameMenu.cpp
@@ -0,0 +1,27 @@
+#include "GameMenu.h"
+#include "fstream"
+
+GameMenu::GameMenu()
+{
+    sSettingsFileName = "Settings.ini";
+    xPlayersCount = 2;
+    xDifficulty = GAME_DIFFICULTY_MEDIUM;
+    sGameType = "Arcade";
+
+    oPlayer[0].sName = "Player1";
+    oPlayer[0].xLeftKeyCode=SDLK_a;
+    oPlayer[0].xRightKeyCode=SDLK_z;
+    oPlayer[0].bIsAIplayer=false;
+
+    oPlayer[1].sName = "Player2";
+    oPlayer[1].xLeftKeyCode=SDLK_l;
+    oPlayer[1].xRightKeyCode=SDLK_p;
+    oPlayer[1].bIsAIplayer=false;
+
+
+}
+
+GameMenu::~GameMenu()
+{
+    //dtor
+}
